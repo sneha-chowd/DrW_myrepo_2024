48d99f02346071ede15a8ecf5aaaf6a8fe27b9b5
sneha-chowd <schowdhury2@luc.edu>
2025-02-12 16:18:52 -0600

    2 liner

--- DNA.py
+++ DNA.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 s = open('rosalind_dna.txt','r').read()
-for n in ["A","C","G","T"]:
-    print(s.count(n), end=' ')
+print(s.count("A"), s.count("C"), s.count("G"), s.count("T"))
+
